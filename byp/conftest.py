04db57f10f2d849bbecb25c9449319af91443958
abhaya <gautamabhaya98@gmail.com>
2024-07-04 01:03:41 +0545

    first commit

---
+++ byp/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from byp.users.models import User
+from byp.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture()
+def user(db) -> User:
+    return UserFactory()
