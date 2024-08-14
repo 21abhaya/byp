d12b5f1046282c5b6069c2d82cf46c1ed6e7874b
abhaya <gautamabhaya98@gmail.com>
2024-08-15 00:04:32 +0545

    defined models for customer

--- customer/models.py
+++ customer/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50, help_text="Enter first name")
+    last_name = models.CharField(max_length=50, help_text="Enter last name")
+    email = models.EmailField(max_length=200, null=True)
+    phone_no = models.CharField(max_length=50, null=True)
+    address = models.CharField(max_length=255, null=True)
+
+
+    def __str__(self):
+
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name
+    
