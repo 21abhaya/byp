67e1cc447d18b4895aa92ca63a77e6e51cbb6fdf
abhaya <gautamabhaya98@gmail.com>
2024-08-16 23:37:38 +0545

    defined models for booking

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from photographer.models import Photographer
+from customer.models import Customer
 
-# Create your models here.
+class Booking(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    scheduled_by = models.OneToOneField(Customer, on_delete=models.SET_NULL, null=True, verbose_name="Client")
+    scheduled_with = models.OneToOneField(Photographer, on_delete=models.SET_NULL, null=True, verbose_name="Photographer")
+    scheduled_on = models.DateTimeField()
+
+    class Meta:
+        abstract = True
+
+class Interview(Booking):
+    TYPE=[
+        ('In person','In person')
+        ('Virtual','Virtual')
+    ]
+    interview_type = models.CharField(max_length=35, choices=TYPE, null=True)
+
+class Photoshoot(Booking):
+    venue = models.CharField(max=255, null=True, help_text="Location of the photoshoot venue")
