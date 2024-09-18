e6181bdc5a2e245dced76809bb03d9f82899bb36
abhaya <gautamabhaya98@gmail.com>
2024-09-18 23:53:50 +0545

    registered models to admin site

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Interview, Photoshoot
 
-# Register your models here.
+admin.site.register(Interview)
+admin.site.register(Photoshoot)
--- photographer/admin.py
+++ photographer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Photographer, Portfolio, Ratings, Review
 
-# Register your models here.
+admin.site.register(Photographer)
+admin.site.register(Portfolio)
+admin.site.register(Ratings)
+admin.site.register(Review)
