b62e805b56e5274c8c896f16c84aab26ef92feaf
abhaya <gautamabhaya98@gmail.com>
2024-11-04 22:13:28 +0545

    fix: media settings configured

--- byp/settings.py
+++ byp/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 
@@ -20,6 +21,8 @@ environ.Env.read_env() # reads .env file
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
