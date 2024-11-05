957b48634a5ac077faa1700d2bacd7c8ecba462b
abhaya <gautamabhaya98@gmail.com>
2024-11-05 16:30:35 +0545

    added: context for views

--- photographer/views.py
+++ photographer/views.py
@@ -1,7 +1,16 @@
 from django.views.generic import ListView, DetailView
-from .models import Photographer
+from .models import Photographer, Portfolio, Ratings
 
 class PhotographerListView(ListView):
     model = Photographer
     template_name = 'photographer/photographer_list.html'
     context_object_name = 'photographers'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['photographers'] = Photographer.objects.all()
+        context['reviews'] = Ratings.objects.all()
+        context['portfolios'] = Portfolio.objects.all()
+        return context
+    
+    
