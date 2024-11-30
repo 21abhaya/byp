aa9b5834c5ee548746f2f72057f058370ad15a69
abhaya <gautamabhaya98@gmail.com>
2024-11-30 23:52:07 +0545

    working: search fail response for photographer list

--- photographer/views.py
+++ photographer/views.py
@@ -7,23 +7,33 @@ class PhotographerListView(ListView):
     template_name = 'photographer/photographer_list.html'
     paginate_by = 12
     
-    def get_queryset(self):
-        queryset = super().get_queryset()
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     search_term = self.request.GET.get('search')
+    #     if search_term:
+    #         queryset=queryset.filter(
+    #             Q(first_name__icontains=search_term)|
+    #             Q(last_name__icontains=search_term)|
+    #             Q(category__icontains=search_term)
+    #         )
+    #     return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        queryset = self.model.objects.all()
         search_term = self.request.GET.get('search')
         if search_term:
             queryset=queryset.filter(
                 Q(first_name__icontains=search_term)|
                 Q(last_name__icontains=search_term)|
                 Q(category__icontains=search_term)
-            )
-        return queryset
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+            ) 
+            if not queryset.exists():
+                context['no_results']=f'No match found for "{search_term}"'  
         context['photographers'] = Photographer.objects.all()
         context['ratings'] = Rating.objects.all()
         context['portfolios'] = Portfolio.objects.all()
-        context['search_term'] = self.request.GET.get('search')
+        context['search_term'] = search_term
         return context
     
 
