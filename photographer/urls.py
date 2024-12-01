44584affe86c252c4c436ce5e0de36b4dc3a982e
abhaya <gautamabhaya98@gmail.com>
2024-12-01 21:26:53 +0545

    working: photographer detail page

--- photographer/urls.py
+++ photographer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PhotographerListView
+from .views import PhotographerListView, PhotographerDetailView
 
 app_name = 'photographer'
 
 urlpatterns=[
     path('', PhotographerListView.as_view(), name='photographers'),
+    path('photographer/<int:pk>/', PhotographerDetailView.as_view(), name='photographer-detail'),
 ]
