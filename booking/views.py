18c8a0cd15919ca7dc222b985f9e164208e52b25
abhaya <gautamabhaya98@gmail.com>
2024-12-13 23:58:22 +0545

    working: photographer booking

--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+from .models import Interview, Photoshoot, Booking
 
-# Create your views here.
+
+
