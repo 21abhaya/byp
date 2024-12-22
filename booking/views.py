ce0b9a02a114caefce4937928943af5261d77f35
abhaya <gautamabhaya98@gmail.com>
2024-12-22 23:44:41 +0545

    trying out cursor

--- booking/views.py
+++ booking/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
-from .models import Interview, Photoshoot, Booking
+from .models import Photographer, Interview, Photoshoot, Booking
 
 
 class InterviewCreateView(CreateView):
     model = Interview
     template_name = 'booking/create_booking.html'
     fields =['scheduled_on']
-    
