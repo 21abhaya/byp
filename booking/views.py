db652b10b6e258e47ccb064e47c524f4767f4724
abhaya <gautamabhaya98@gmail.com>
2024-12-25 23:54:53 +0545

    working: Booking views

--- booking/views.py
+++ booking/views.py
@@ -7,3 +7,10 @@ class InterviewCreateView(CreateView):
     model = Interview
     template_name = 'booking/create_booking.html'
     fields =['scheduled_on']
+    context_object_name = 'booking'
+    obj_id = id
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['photographer'] = Photographer.objects.all()
+        return context
