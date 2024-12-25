cae0f3981f7f64fcf3a0ce8edb9e354aedd2ac12
abhaya <gautamabhaya98@gmail.com>
2024-12-26 00:12:37 +0545

    i suck, need to get better

--- booking/views.py
+++ booking/views.py
@@ -8,9 +8,3 @@ class InterviewCreateView(CreateView):
     template_name = 'booking/create_booking.html'
     fields =['scheduled_on']
     context_object_name = 'booking'
-    obj_id = id
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['photographer'] = Photographer.objects.all()
-        return context
