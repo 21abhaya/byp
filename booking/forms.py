c995c81d3c78cad4585f9eb0689fd40482f23958
abhaya <gautamabhaya98@gmail.com>
2025-01-18 23:11:01 +0545

    work: basic form for booking a photographer

--- booking/forms.py
+++ booking/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.forms import ModelForm
 from .models import Interview, Photoshoot, Photographer
 
-
+class InterviewCreateForm(forms.ModelForm):
+    model = Interview
+    fields='__all__'
+    
--- booking/views.py
+++ booking/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
 from .models import Photographer, Interview, Photoshoot, Booking
-
+from .forms import InterviewCreateForm
 
 class InterviewCreateView(CreateView):
     model = Interview
     template_name = 'booking/create_booking.html'
+    form = InterviewCreateForm
+    fields = '__all__'
